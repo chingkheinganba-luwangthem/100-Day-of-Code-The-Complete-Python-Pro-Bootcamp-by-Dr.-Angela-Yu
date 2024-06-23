7b13bf997339fedf9a14ac1fecfd0dccbb97dbda
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2024-06-23 22:54:47 +0530

    Update hangman_words.py

--- Day7-Project-Hangman_Game/hangman_words.py
+++ Day7-Project-Hangman_Game/hangman_words.py
@@ -1,1 +1,14 @@
+word_list = [
+    'python', 'javascript', 'hangman', 'developer', 'keyboard',
+    'monitor', 'laptop', 'notebook', 'function', 'variable',
+    'constant', 'integer', 'boolean', 'character', 'syntax',
+    'compile', 'execute', 'debugger', 'algorithm', 'binary',
+    'decimal', 'hexadecimal', 'operator', 'operand', 'array',
+    'matrix', 'vector', 'string', 'substring', 'dictionary',
+    'iteration', 'recursion', 'inheritance', 'polymorphism',
+    'encapsulation', 'abstraction', 'interface', 'exception',
+    'handler', 'parameter', 'argument', 'class', 'object',
+    'instance', 'constructor', 'destructor', 'namespace',
+    'scope', 'global', 'local'
+]
 
