f1af9e515ba9e7e7a1dbc2fcc7879e26121ed92e
Chingkheinganba Luwangthem <158492255+chingkheinganba-luwangthem@users.noreply.github.com>
2024-06-26 08:00:12 +0530

    Update clear_screen.py

--- Day9-Project-Silent_Auction_Projram/clear_screen.py
+++ Day9-Project-Silent_Auction_Projram/clear_screen.py
@@ -1,1 +1,12 @@
+import os
 
+def clear():
+    # Check if the operating system is Windows
+    if os.name == 'nt':
+        # For Windows
+        os.system('cls')
+    else:
+        # For Linux and Mac
+        os.system('clear')
+
+        os.system('clear')
